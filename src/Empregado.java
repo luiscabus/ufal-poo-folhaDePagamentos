d16cf3b34c8bb96fa99d5cd979df0c663e26e121
Luís Alberto <luiscabus@gmail.com>
2018-11-28 01:22:13 +0000

    Major changes and working prototype

--- src/Empregado.java
+++ src/Empregado.java
@@ -1,3 +1,6 @@
+
+
+
 import java.util.*;
 
 
@@ -8,32 +11,165 @@ public class Empregado {
     String endereco;
     int tipo; //1 hourly, 2 salaried, 3 commissioned
     int salarioHora;
-    int salarioMes;
+    float salarioMes;
     int comissao;
+
+    int tipoDeAgenda; // 1 1x/mes, 2 2x/mes, 4 4x/mes...
+    int metodoPagamento; // 1 cheque correio, 2 cheque maos, 3 conta bancaria
+
+    int pertenceSindicato;
+    float taxaSindicato;
+
+    float salarioHoraAcumulado;
+    float comissoesAcumuladas;
+    float taxaSindAcumulado;
+
 
     public Empregado(){}
 
-    public Empregado(String nome, String endereco, int tipo, int salarioHora, int salarioMes, int comissao) {
-        this.id = 1;
+    public Empregado(int id, String nome, String endereco, int tipo, int hora, int mes, int comissao, int agenda, int metodo, int pertence, float taxasind) {
+        this.id = id;
         this.nome = nome;
         this.endereco = endereco;
         this.tipo = tipo;
-        this.salarioHora = salarioHora;
-        this.salarioMes = salarioMes;
+        this.salarioHora = hora;
+        this.salarioMes = mes;
         this.comissao = comissao;
+        this.tipoDeAgenda = agenda;
+        this.metodoPagamento = metodo;
+        this.pertenceSindicato = pertence;
+        this.taxaSindicato = taxasind;
     }
 
-    public void PerguntarDados () {
+    public void PerguntarDados (int novo_emp_id) {
 
+        this.setEmpregadoId(novo_emp_id);
+
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Entre com o nome do empregado:\n");
-        try (Scanner scanner = new Scanner(System.in)) {
-            this.SetEmpregadoNome(scanner.nextLine());
-            scanner.nextLine();
-            System.out.println("Novo empregado: " + this.nome);
-        } catch (Exception e) {
-            e.printStackTrace(System.err);
+        this.SetEmpregadoNome(scanner.nextLine());
+
+        System.out.print("Entre com o endereço:\n");
+        this.SetEmpregadoEndereco(scanner.nextLine());
+
+        System.out.print("Entre com o tipo do empregado (1 hourly, 2 salaried, 3 comissioned):\n");
+        this.SetEmpregadoTipo(scanner.nextInt());
+
+        if (this.getEmpregadoTipo() == 1) {
+            System.out.print("Entre com o salário hora:\n");
+            this.SetEmpregadoSalarioH(scanner.nextInt());
+            this.SetEmpregadoTipodeAgenda(4);
         }
 
+        if (this.getEmpregadoTipo() == 2) {
+            System.out.print("Entre com o salário mensal:\n");
+            this.SetEmpregadoSalarioM(scanner.nextInt());
+            this.SetEmpregadoTipodeAgenda(1);
+        }
+
+        if (this.getEmpregadoTipo() == 3) {
+            System.out.print("Entre com o salário parte fixa mensal:\n");
+            this.SetEmpregadoSalarioM(scanner.nextInt());
+            System.out.print("Entre com a comissão (%):\n");
+            this.SetEmpregadoSalarioC(scanner.nextInt());
+            this.SetEmpregadoTipodeAgenda(2);
+        }
+
+        System.out.print("Pertence a sindicato? (0 não, 1 sim):\n");
+        this.SetEmpregadoPertenceSindicato(scanner.nextInt());
+
+        if (this.pertenceSindicato == 1) {
+            System.out.print("Qual a taxa mensal cobrada pelo sindicato?\n");
+            this.taxaSindicato = scanner.nextFloat();
+        }
+
+        System.out.println("Novo empregado incluído: ID: " + novo_emp_id + ", Nome: " + this.nome + ", Endereço: " + this.endereco);
+
+    }
+
+    public void ExcluirEmpregado (ArrayList<Empregado> arrayEmpregados) {
+
+        System.out.println("Conforme a lista abaixo, qual empregado você deseja excluir? (opção)");
+
+        for (int i = 0; i < arrayEmpregados.size(); ++i) {
+            System.out.printf("(%d) Nome: %s\n", i, arrayEmpregados.get(i).nome);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int indice = scanner.nextInt();
+
+        arrayEmpregados.remove(indice);
+
+        System.out.println("Lista de empregados atualizada:");
+        for (int i = 0; i < arrayEmpregados.size(); ++i) {
+            System.out.printf("ID: %d, Nome: %s\n", i, arrayEmpregados.get(i).id, arrayEmpregados.get(i).nome);
+        }
+    }
+
+    public void LancarCartaodePonto(ArrayList<Empregado> arrayEmpregados) {
+        System.out.println("Qual empregado vai lançar horas? (opção) [horista tem tipo 1]");
+        for (int i = 0; i < arrayEmpregados.size(); ++i) {
+            System.out.printf("(%d) Nome: %s, Tipo: %d\n", i, arrayEmpregados.get(i).nome, arrayEmpregados.get(i).tipo);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int selecionado = scanner.nextInt();
+
+        if (arrayEmpregados.get(selecionado).tipo != 1) {
+            System.out.println("Lascou, não é horista");
+        } else {
+            System.out.printf("Quantas horas o empregado %s trabalhou\n", arrayEmpregados.get(selecionado).nome);
+            int horas_trabalhadas = scanner.nextInt();
+            float salario_add = 0;
+
+            if (horas_trabalhadas > 8) {
+                salario_add += arrayEmpregados.get(selecionado).salarioHoraAcumulado + arrayEmpregados.get(selecionado).salarioHora * 8;
+                salario_add += arrayEmpregados.get(selecionado).salarioHoraAcumulado + arrayEmpregados.get(selecionado).salarioHora * (horas_trabalhadas - 8) * 1.5;
+            } else {
+                salario_add += arrayEmpregados.get(selecionado).salarioHoraAcumulado + arrayEmpregados.get(selecionado).salarioHora * horas_trabalhadas;
+            }
+            arrayEmpregados.get(selecionado).salarioHoraAcumulado += salario_add;
+        }
+
+    }
+
+    public void LancarVenda(ArrayList<Empregado> arrayEmpregados) {
+        System.out.println("Qual empregado vai lançar venda? (opção) [comissionado tem tipo 3]");
+        for (int i = 0; i < arrayEmpregados.size(); ++i) {
+            System.out.printf("(%d) Nome: %s, Tipo: %d\n", i, arrayEmpregados.get(i).nome, arrayEmpregados.get(i).tipo);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int selecionado = scanner.nextInt();
+
+        if (arrayEmpregados.get(selecionado).tipo != 3) {
+            System.out.printf("Eita, esse %s não é comissionado.", arrayEmpregados.get(selecionado).nome);
+        } else {
+            System.out.printf("Qual valor da venda para lançar ao empregado %s?\n", arrayEmpregados.get(selecionado).nome);
+            int entrada = scanner.nextInt();
+            arrayEmpregados.get(selecionado).comissoesAcumuladas += entrada * arrayEmpregados.get(selecionado).comissao / 100;
+        }
+    }
+
+    public void LancarTaxas(ArrayList<Empregado> arrayEmpregados) {
+        System.out.println("Qual empregado vai lançar taxas de sindicato? (aqui)");
+        for (int i = 0; i < arrayEmpregados.size(); ++i) {
+            System.out.printf("(%d) Nome: %s, Sindicalizado: %d\n", i, arrayEmpregados.get(i).nome, arrayEmpregados.get(i).pertenceSindicato);
+        }
+
+        Scanner scanner = new Scanner(System.in);
+        int selecionado = scanner.nextInt();
+
+        if (arrayEmpregados.get(selecionado).tipo != 1) {
+            System.out.println("Lascou, não pertence a sindicato");
+        } else {
+            System.out.printf("Qual o valor da taxa a ser lançada para o empregado %s?\n", arrayEmpregados.get(selecionado).nome);
+            int entrada = scanner.nextInt();
+            arrayEmpregados.get(selecionado).taxaSindAcumulado += entrada;
+        }
+    }
+
+    public void GerarFolha(ArrayList<Empregado> arrayEmpregados) {
 
     }
 
@@ -50,15 +186,20 @@ public class Empregado {
     public int getEmpregadoSalarioH() {
         return salarioHora;
     }
-    public int getEmpregadoSalarioM() {
+    public float getEmpregadoSalarioM() {
         return salarioMes;
     }
     public int getEmpregadoSalarioC() {
         return comissao;
     }
+    public int getEmpregadoPertenceSindicato() {
+        return pertenceSindicato;
+    }
 
 
-
+    public void setEmpregadoId(int id) {
+        this.id = id;
+    }
     public void SetEmpregadoNome(String nome) {
         this.nome = nome;
     }
@@ -71,11 +212,17 @@ public class Empregado {
     public void SetEmpregadoSalarioH(int salarioHora) {
         this.salarioHora = salarioHora;
     }
-    public void SetEmpregadoSalarioM(int salarioMes) {
+    public void SetEmpregadoSalarioM(float salarioMes) {
         this.salarioMes = salarioMes;
     }
     public void SetEmpregadoSalarioC(int comissao) {
         this.comissao = comissao;
+    }
+    public void SetEmpregadoPertenceSindicato(int pertenceSindicato) {
+        this.pertenceSindicato = pertenceSindicato;
+    }
+    public void SetEmpregadoTipodeAgenda(int tipoDeAgenda) {
+        this.tipoDeAgenda = tipoDeAgenda;
     }
 
 
--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+
+
 //javac -d /projects/folhaDePagamentos/bin/ /projects/folhaDePagamentos/src/*.java
 //java Main /projects/folhaDePagamentos/bin/Main.class
 //https://www.caelum.com.br/apostila-java-orientacao-objetos/orientacao-a-objetos-basica/#exerccios-orientao-a-objetos
@@ -7,42 +9,282 @@ import java.util.*;
 public class Main {
 
     public static void main(String[] args) {
-        // write your code here
-        System.out.println("Hello World!");
-        System.out.println("Este sistema é uma folha de pagamentos!");
+        System.out.println();System.out.println();
 
-        java.util.ArrayList<Empregado> meusEmpregados = new java.util.ArrayList<Empregado>();
+        int emp_count = 0;
+        int dia_de_hoje = 1;
+        int mes_de_hoje = 1;
+        int ano_de_hoje = 2018;
+        int diasCorridos = 2;
+        String diaDaSemana = "";
 
-        int index;
+        ArrayList<Empregado> meusEmpregadosList = new ArrayList<Empregado>();
+
+        emp_count+=1;
+        Empregado novoEmp = null;
+        novoEmp = new Empregado(emp_count, "HH Horista", "HOR", 1, 12, 0, 0, 4, 3, 0, 0);
+        meusEmpregadosList.add(novoEmp);
+
+        emp_count+=1;
+        novoEmp = new Empregado(emp_count, "AA Assalariado", "ASS", 2, 0, 964, 0, 1, 3,0, 0);
+        meusEmpregadosList.add(novoEmp);
+
+        emp_count+=1;
+        novoEmp = new Empregado(emp_count, "CC Comissionado", "COM", 3, 0, 600, 12, 2, 3, 0, 0);
+        meusEmpregadosList.add(novoEmp);
+
+        emp_count+=1;
+        novoEmp = new Empregado(emp_count, "HORIST Sindical", "HOR SIN Endereço", 1, 12, 0, 0, 4, 1, 1, 7);
+        meusEmpregadosList.add(novoEmp);
+
+        emp_count+=1;
+        novoEmp = new Empregado(emp_count, "ASSALA Sindical", "ASS", 2, 0, 964, 0, 1, 3, 1, 8);
+        meusEmpregadosList.add(novoEmp);
+
+        // adicionar horas ao hh horista
+        meusEmpregadosList.get(0).salarioHoraAcumulado += meusEmpregadosList.get(0).salarioHora * 3;
+        meusEmpregadosList.get(0).salarioHoraAcumulado += meusEmpregadosList.get(0).salarioHora * 4;
+
+        // adicionar comissão ao comissionado
+        meusEmpregadosList.get(2).comissoesAcumuladas += meusEmpregadosList.get(2).comissao * 220.00 / 100.00;
+        meusEmpregadosList.get(2).comissoesAcumuladas += meusEmpregadosList.get(2).comissao * 438.00 / 100.00;
+
+        // adicionar horas taxas ao horista sindicalizado
+        meusEmpregadosList.get(3).salarioHoraAcumulado += meusEmpregadosList.get(3).salarioHora * 4;
+        meusEmpregadosList.get(3).taxaSindAcumulado += 13;
+        meusEmpregadosList.get(3).taxaSindAcumulado += 10;
+        meusEmpregadosList.get(3).taxaSindAcumulado += 16;
+
+
+        int index = 0;
         Scanner scanner = new Scanner(System.in);
         do {
-            System.out.print("O que você deseja fazer?\n");
-            System.out.print("1- Adição de empregado\n");
-            System.out.print("2- Remoção de empregado\n");
-            System.out.print("3- Lançar um Cartão de Ponto\n");
-            System.out.print("4- Lançar um Resultado Venda\n");
-            System.out.print("5- Lançar uma taxa de serviço\n");
-            System.out.print("6- Alterar detalhes de um empregado\n");
-            System.out.print("7- Rodar a folha de pagamento para hoje\n");
-            System.out.print("8- Undo\n");
-            System.out.print("9- Redo\n");
-            System.out.print("10- Agenda de Pagamento\n");
-            System.out.print("11- Criação de Novas Agendas de Pagamento?\n");
-            System.out.print("12- Listar empregados\n");  
-            System.out.print("99- SAIR\n");
+
+            switch (diasCorridos % 7) {
+                case 1:
+                    diaDaSemana = "Domingo";
+                    break;
+                case 2:
+                    diaDaSemana = "Segunda-feira";
+                    break;
+                case 3:
+                    diaDaSemana = "Terça-feira";
+                    break;
+                case 4:
+                    diaDaSemana = "Quarta-feira";
+                    break;
+                case 5:
+                    diaDaSemana = "Quinta-feira";
+                    break;
+                case 6:
+                    diaDaSemana = "Sexta-feira";
+                    break;
+                case 0:
+                    diaDaSemana = "Sábado";
+                    break;
+            }
+
             
+
+            System.out.printf("### ---- DIA DE HOJE: %d/%d/%d -- %s ---- ###    ", dia_de_hoje, mes_de_hoje, ano_de_hoje, diaDaSemana);
+            System.out.printf("Esta é a gambiarra de uma folha de pagamentos!\n\n");
+            System.out.print("O que você deseja fazer?\n");
+            System.out.print("1- Adição de empregado\t\t\t\t");
+            System.out.print("2- Remoção de empregado\t\t\t\t");
+            System.out.print("6- Alterar detalhes de um empregado\t\t");
+            System.out.print("10- Alterar agenda de pagamento\n");
+
+            System.out.print("3- Lançar um Cartão de Ponto\t\t");
+            System.out.print("4- Lançar um Resultado Venda\t\t");
+            System.out.print("5- Lançar uma taxa de serviço\n");
+
+            System.out.print("7- Rodar a folha de pagamento para hoje\n");
+            System.out.print("8- Undo\t\t");
+            System.out.print("9- Redo\n");
+            System.out.print("11- Criação de Novas Agendas de Pagamento\n");
+            System.out.print("12- Listar empregados cadastrados\n");
+            System.out.print("99- SAIR:\n");
+
             index = scanner.nextInt();
             System.out.println("Sua escolha foi: " + index + "\t");
 
-            if (index == 1) {
+            if (index == 1) { //adicionar empregado
                 Empregado novoEmpregado = new Empregado();
-                novoEmpregado.PerguntarDados();
-                meusEmpregados.add(novoEmpregado);
+                emp_count += 1;
+                novoEmpregado.PerguntarDados(emp_count);
+                meusEmpregadosList.add(novoEmpregado);
+            }
+
+            if (index == 2) { //remover empregado
+                System.out.println();System.out.println();
+                new Empregado().ExcluirEmpregado(meusEmpregadosList);
+                System.out.println();System.out.println();
+            }
+
+            if (index == 3) { //inserir cartao de ponto
+                System.out.println();System.out.println();
+                new Empregado().LancarCartaodePonto(meusEmpregadosList);
+                System.out.println();System.out.println();
+            }
+
+            if (index == 4) { //inserir venda
+                System.out.println();System.out.println();
+                new Empregado().LancarVenda(meusEmpregadosList);
+                System.out.println();System.out.println();
+            }
+
+            if (index == 5) { //lançar taxa de serviços de sindicato
+                System.out.println();System.out.println();
+                new Empregado().LancarTaxas(meusEmpregadosList);
+                System.out.println();System.out.println();
+            }
+
+            if (index == 6) {
+
+            }
+
+            if (index == 7) {
+                System.out.println();System.out.println("# Folha de Pagamentos");
+
+                if (dia_de_hoje == 30) { // mensalistas
+                    for (int i = 0; i < meusEmpregadosList.size(); ++i) {
+                        if (meusEmpregadosList.get(i).tipoDeAgenda == 1) {
+                            if (meusEmpregadosList.get(i).tipo == 2) {
+                                System.out.println("=---");
+                                String formapagamento = "";
+                                if (meusEmpregadosList.get(i).metodoPagamento == 1) {
+                                    formapagamento = "Cheque pelos correios para o endereço: " + meusEmpregadosList.get(i).endereco;
+                                }
+                                if (meusEmpregadosList.get(i).metodoPagamento == 2) {
+                                    formapagamento = "Cheque entregue em mãos";
+                                }
+                                if (meusEmpregadosList.get(i).metodoPagamento == 3) {
+                                    formapagamento = "Depósito em conta bancária";
+                                }
+
+                                float saldoLiquido = meusEmpregadosList.get(i).salarioMes - meusEmpregadosList.get(i).taxaSindicato - meusEmpregadosList.get(i).taxaSindAcumulado;
+
+                                System.out.println("=---");
+                                System.out.printf("[%s]\n", meusEmpregadosList.get(i).nome);
+                                System.out.printf("Salário bruto [%.2f]\n", meusEmpregadosList.get(i).salarioHoraAcumulado);
+                                System.out.printf("- Desconto do Sindicato [%.2f]\n", meusEmpregadosList.get(i).taxaSindicato);
+                                System.out.printf("- Taxas do Sindicato [%.2f]\n", meusEmpregadosList.get(i).taxaSindAcumulado);
+                                System.out.printf("== Valor Líquido a Receber [%.2f]\n", saldoLiquido);System.out.println();
+                                System.out.printf("Realizar o pagamento na forma:\n[%s]\n", formapagamento);
+                                System.out.println("---=");
+
+                                meusEmpregadosList.get(i).taxaSindAcumulado = 0;
+                            }
+                        }
+                    }
+                }
+
+                if (diasCorridos % 7 == 6) { //sexta-feira para horistas
+                    for (int i = 0; i < meusEmpregadosList.size(); ++i) {
+                        if (meusEmpregadosList.get(i).tipoDeAgenda == 4) {
+                            if (meusEmpregadosList.get(i).tipo == 1 ) { //horista
+
+                                String formapagamento = "";
+                                if (meusEmpregadosList.get(i).metodoPagamento == 1) {
+                                    formapagamento = "Cheque pelos correios para o endereço: " + meusEmpregadosList.get(i).endereco;
+                                }
+                                if (meusEmpregadosList.get(i).metodoPagamento == 2) {
+                                    formapagamento = "Cheque entregue em mãos";
+                                }
+                                if (meusEmpregadosList.get(i).metodoPagamento == 3) {
+                                    formapagamento = "Depósito em conta bancária";
+                                }
+
+                                float saldoLiquido = meusEmpregadosList.get(i).salarioHoraAcumulado - meusEmpregadosList.get(i).taxaSindicato - meusEmpregadosList.get(i).taxaSindAcumulado;
+
+                                System.out.println("=---");
+                                System.out.printf("[%s]\n", meusEmpregadosList.get(i).nome);
+                                System.out.printf("Salário bruto [%.2f]\n", meusEmpregadosList.get(i).salarioHoraAcumulado);
+                                System.out.printf("- Desconto do Sindicato [%.2f]\n", meusEmpregadosList.get(i).taxaSindicato);
+                                System.out.printf("- Taxas do Sindicato [%.2f]\n", meusEmpregadosList.get(i).taxaSindAcumulado);
+                                System.out.printf("== Valor Líquido a Receber [%.2f]\n", saldoLiquido);System.out.println();
+                                System.out.printf("Realizar o pagamento na forma:\n[%s]\n", formapagamento);
+                                System.out.println("---=");
+
+                                meusEmpregadosList.get(i).salarioHoraAcumulado = 0;
+                                meusEmpregadosList.get(i).taxaSindAcumulado = 0;
+                            }
+                        }
+                    }
+                }
+
+
+                else if (dia_de_hoje == 30) {
+
+                    if (mes_de_hoje == 12) {
+                        mes_de_hoje = 1;
+                        ano_de_hoje++;
+                    } else {
+                        mes_de_hoje++;
+                    }
+                }
+
+
+
+                if (dia_de_hoje < 30) {
+                    dia_de_hoje++;
+                } else if (dia_de_hoje == 30) {
+                    dia_de_hoje = 1;
+                    if (mes_de_hoje == 12) {
+                        mes_de_hoje = 1;
+                        ano_de_hoje++;
+                    } else {
+                        mes_de_hoje++;
+                    }
+                }
+                diasCorridos++;
+                System.out.println();System.out.println();
+            }
+
+            if (index == 12) {
+                System.out.println();System.out.println("# Lista de Empregados");
+                for (int i = 0; i < meusEmpregadosList.size(); ++i) {
+                    System.out.printf("" +
+                                    "ID: %d, " +
+                                    "Nome: %s, " +
+                                    "Tipo %d, " +
+                                    "SH %d, " +
+                                    "SM %.2f, " +
+                                    "SC %d, " +
+
+                                    "Agenda %d, " +
+                                    "MetPag %d, " +
+
+                                    "Sind? %d, " +
+                                    "TaxaSind %.2f, " +
+
+                                    "SalHoraAcum %.2f, " +
+                                    "ComissoesAcum %.2f, " +
+                                    "TxSAcum %.2f\n",
+                            meusEmpregadosList.get(i).id,
+                            meusEmpregadosList.get(i).nome,
+                            meusEmpregadosList.get(i).tipo,
+                            meusEmpregadosList.get(i).salarioHora,
+                            meusEmpregadosList.get(i).salarioMes,
+                            meusEmpregadosList.get(i).comissao,
+
+                            meusEmpregadosList.get(i).tipoDeAgenda,
+                            meusEmpregadosList.get(i).metodoPagamento,
+
+                            meusEmpregadosList.get(i).pertenceSindicato,
+                            meusEmpregadosList.get(i).taxaSindicato,
+
+                            meusEmpregadosList.get(i).salarioHoraAcumulado,
+                            meusEmpregadosList.get(i).comissoesAcumuladas,
+                            meusEmpregadosList.get(i).taxaSindAcumulado);
+                }
+                System.out.println();System.out.println();
             }
 
         } while (index != 99);
-        scanner.close();
+
 
 
     }
+
 }
